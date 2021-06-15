4063b1a547b8624366eafdda9b4d435746760a25
jaredg-ai <jaredward.harod@gmail.com>
2021-06-15 12:22:47 -0600

    added express

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ let rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log("Hello world!");
 
+const app = express()
+app.use(express.json())
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully')
