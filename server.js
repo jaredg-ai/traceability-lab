ec21975411e9fbb1b36598c5af3a0d49469b22c9
jaredg-ai <jaredward.harod@gmail.com>
2021-06-15 12:09:37 -0600

    added rollbar

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require('express')
 const path = require('path')
+// include and initialize the rollbar library with your access token
+const Rollbar = require("rollbar");
+let rollbar = new Rollbar({
+  accessToken: '513d2bb205234c3489594eba7d5f0056',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
 
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
 
